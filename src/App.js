0fa2a2d58201c74cb8661582dd2828018f5e7ed6
nasirmondol <nasirahmed8202@gmail.com>
2022-11-15 09:08:35 +0600

    header implement

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Home from './pages/Home/Home/Home';
 import NotFound from './pages/NotFound/NotFound';
 import Header from './pages/Shared/Header/Header';
 import Footer from './pages/Shared/Footer/Footer';
+import Blogs from './pages/Home/Blogs/Blogs';
+import Login from './pages/Login/Login/Login';
+import Register from './pages/Login/Register/Register';
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}></Route>
         <Route path='/home' element={<Home/>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
         <Route path='*' element={<NotFound/>}></Route>
       </Routes>
       <Footer/>
--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './Header.css'
+import logo from '../../../images/HeaderLogo/header logo.png' 
 
 const Header = () => {
     return (
         <div>
-            <h1>This is header compo</h1>
+            <Navbar collapseOnSelect fixed='top' expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand as={Link} to="/home">
+            <img className='rounded-circle' src={logo} alt="" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto font-weight-bold">
+            <Nav.Link as={Link} to="/home">Home</Nav.Link>
+            <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
+            
+          </Nav>
+          <Nav>
+            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+            <Nav.Link as={Link} to="/register">
+              Sign Up
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
         </div>
     );
 };
